bf2520df05abcfd80874979557c2184230c005a7
Harsha Attray <harsh.attray@gmail.com>
2017-09-19 10:35:30 +0530

    More Array helper functions

---
+++ JS/EveryandSomehelper.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: Harsha Attray <harsha>
+ * @Date:   2017-09-17T23:24:45+05:30
+ * @Project: Retailstore-Attendance-Monitor
+ * @Filename: EveryandSomehelper.js
+ * @Last modified by:   harsha
+ * @Last modified time: 2017-09-17T23:30:45+05:30
+ */
+var cars = [
+  {name :'electric', speed: 13},
+  {name :'diesel', speed: 130},
+  {name :'Fuelcell', speed: 300}
+
+]
