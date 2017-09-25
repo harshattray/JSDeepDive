9c688b803e8ff343ff9674f4f96bfd30acddeaf4
Harsha Attray <harsh.attray@gmail.com>
2017-09-25 11:08:25 +0530

    forSome/forEvery

--- JS/EveryandSomehelper.js
+++ JS/EveryandSomehelper.js
@@ -4,11 +4,29 @@
  * @Project: Retailstore-Attendance-Monitor
  * @Filename: EveryandSomehelper.js
  * @Last modified by:   harsha
- * @Last modified time: 2017-09-17T23:30:45+05:30
+ * @Last modified time: 2017-09-22T17:42:07+05:30
  */
 var cars = [
   {name :'electric', speed: 13},
   {name :'diesel', speed: 130},
   {name :'Fuelcell', speed: 300}
-
 ]
+//using for loop
+
+var allMachinesCanRunProg = true;
+var onlySomeMachinesCanRunProg = false
+
+for(var i = 0; i< cars.length i ++ ){
+  var car = cars[i];
+
+  if(car.speed < 100){
+    allMachinesCanRunProg = false;
+  }
+  else{
+   onlySomeMachinesCanRunProg = true
+  }
+}
+
+/*forEvery runs through each of the values in the array and checks for truthy or falsy condition of each one of them  individually
+ and also combined together and returns a boolean value 
+*/
