d496e067aa7790b07b3158ed67f584325c2fa3db
harshattray <harsh.attray@gmail.com>
2017-09-28 17:36:50 +0530

    Deeper Destructuring

--- JS/Es6Destructuring.js
+++ JS/Es6Destructuring.js
@@ -2,7 +2,7 @@
  * @Author: harsha
  * @Date:   2017-09-28T17:09:02+05:30
  * @Last modified by:   harsha
- * @Last modified time: 2017-09-28T17:14:03+05:30
+ * @Last modified time: 2017-09-28T17:36:21+05:30
  */
 
 var expense = {
@@ -16,3 +16,34 @@ var amt = expense.amount;
 
 /*ES6*/
 const { type, amount } = expense
+
+
+/**/
+
+var savedFile = {
+  extension : '.png',
+  name : 'my_image',
+  size: 98089
+}
+
+function filedetailsFetch(file) {
+  return `The file ${file.name}.${file.extension} is of size ${file.size}`;
+}
+
+filedetailsFetch(savedFile);
+
+/*Destructured Version*/
+
+function  filedetailsFetchDes({name,extension,size}){
+  return `The file ${name}.${extension} is of size ${size}`
+}
+
+filedetailsFetchDes(savedFile);
+
+/*with Multiple Arguments*/
+
+function  filedetailsFetchDesMul({name,extension,size},{shape}){
+  return `The file ${name}.${extension} is of size ${size} and shape ${shape}`
+}
+
+filedetailsFetchDes(savedFile,{shape:'oval'});
